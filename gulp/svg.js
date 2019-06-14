d832595b05b9ea2a73fad1ba42b20520c57e1a7c
Constantine Lebedyuk <constantinelebedyuk@gmail.com>
2019-06-14 10:29:53 +0300

    New commit...

---
+++ gulp/svg.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const svgmin = require('gulp-svgmin');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
+const svgSprite = require('gulp-svg-sprite');
+
+gulp.task('svg', function () {
+    return gulp.src('src/img/**/*.svg')
+        .pipe(svgmin({
+            js2svg: {
+                pretty: true
+            }
+        }))
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[style]').removeAttr('style');
+            },
+            parserOptions: {xmlMode: true}
+        }))
+        .pipe(replace('&gt;', '>'))
+        // build svg sprite
+        .pipe(svgSprite({
+            mode: {
+                symbol: {
+                    sprite: 'sprite.svg'
+                }
+            }
+        }))
+        .pipe(gulp.dest('frontend/img'));
+});
